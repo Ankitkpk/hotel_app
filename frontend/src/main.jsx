5e2055faf65ecf4691930e477b6a2e55fda5b8ed
ankit <ankitkarapurkar444@gmail.com>
2025-05-23 08:03:22 -0700

    project completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-import AppContextProvider from './context/appContext.jsx'
+import {AppContextProvider} from './context/appContext.jsx'
 import {BrowserRouter} from 'react-router-dom';
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
